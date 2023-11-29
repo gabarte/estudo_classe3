3bb2b513913ea9a797514d7286ad345856e7ab02
gabarte <129518419+gabarte@users.noreply.github.com>
2023-11-29 17:52:45 -0300

    Add files via upload

---
+++ Aluno.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_classe3_aluno
+{
+    class Aluno
+    {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+        public double somatoria;
+
+        public double SomaNota()
+        {
+            somatoria = Nota1 + Nota2 + Nota3;
+            return somatoria;
+        }
+
+        public string Aprovacao()
+        {
+            if (SomaNota() >= 60)
+            {
+                return "APROVADO";
+            }
+            else
+            {
+                double total = 60 - somatoria;
+                Console.WriteLine("FALTARAM: " + total + " PONTOS");
+                return "REPROVADO";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using ex_classe3_aluno;
+
+Aluno a = new Aluno();
+
+Console.Write("Digite o nome do aluno: ");
+a.Nome = Console.ReadLine();
+
+Console.WriteLine();
+Console.WriteLine("Digite as três notas do aluno");
+Console.Write("1º Semestre: 30/");
+a.Nota1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine();
+Console.Write("2º Semestre: 35/");
+a.Nota2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine();
+Console.Write("3º Semestre: 35/");
+a.Nota3 = double.Parse(Console.ReadLine());
+
+Console.WriteLine();
+double somaNota = a.SomaNota();
+Console.WriteLine("A nota total é: " + somaNota);
+
+string aprovado = a.Aprovacao();
+Console.WriteLine(aprovado);
